25a585bb3e0b38de7569d6ffa7240dd4222830b9
João Carmo <17721880+joaocarmo@users.noreply.github.com>
2021-02-17 22:36:21 +0000

    Added more tests

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,11 @@
-/* eslint-disable max-len */
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
+  collectCoverage: true,
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/lib/__mocks__/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
   },
+  resetMocks: false,
   setupFilesAfterEnv: ['jest-enzyme', '<rootDir>/jest.setup.js'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   testEnvironment: 'enzyme',
