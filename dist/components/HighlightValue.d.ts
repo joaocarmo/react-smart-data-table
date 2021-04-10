43045185403c913b8007b7353c77e035b9616347
João Carmo <17721880+joaocarmo@users.noreply.github.com>
2021-04-10 18:48:15 +0100

    Removed exporting typings

--- dist/components/HighlightValue.d.ts
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react';
-interface HighlightValueProps {
-    children: ReactNode;
-    filterValue: string;
-}
-declare const _default: import("react").NamedExoticComponent<HighlightValueProps>;
-export default _default;
